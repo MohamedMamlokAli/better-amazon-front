30cd6f77852a097a5f9fcbe71092fe44aa84e8ee
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-10-26 08:27:18 +0200

    ADJUST overflow bug on home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  menuOpened = false;
   title = 'BetterAmazonFront';
+  toggleMenu() {
+    this.menuOpened = !this.menuOpened;
+    console.log(this.menuOpened);
+  }
 }
