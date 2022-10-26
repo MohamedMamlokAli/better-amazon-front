f44d1367d8411f52a3598673eef1b215322194d5
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-10-26 09:17:26 +0200

    ADD filters component

--- src/app/pages/products-page/products-page.module.ts
+++ src/app/pages/products-page/products-page.module.ts
@@ -4,9 +4,10 @@ import { ProductsComponent } from './Products/products.component';
 import { SwiperModule } from 'swiper/angular';
 import { CardComponent } from './card/card.component';
 import { SharedModule } from '../../shared/shared/shared.module';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
-  declarations: [ProductsComponent, CardComponent],
+  declarations: [ProductsComponent, CardComponent, FiltersComponent],
   imports: [CommonModule, SwiperModule, SharedModule],
 })
 export class ProductsPageModule {}
